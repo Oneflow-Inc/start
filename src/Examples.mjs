d873cfabad4dfc4d550e00cb8736319a62736126
tsai <jackalcooper@gmail.com>
2021-08-19 09:30:37 +0800

    rm

--- src/Examples.mjs
+++ src/Examples.mjs
@@ -9,19 +9,9 @@ function $$default(props) {
                 }, "`src/Examples.res`"));
 }
 
-function getServerSideProps(_ctx) {
-  return Promise.resolve({
-              props: {
-                msg: "This page was rendered with getServerSideProps. You can find the source code here: ",
-                href: "https://github.com/Oneflow-Inc/oneflow-start"
-              }
-            });
-}
-
 export {
   $$default ,
   $$default as default,
-  getServerSideProps ,
   
 }
 /* react Not a pure module */
